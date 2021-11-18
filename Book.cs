19912797dbd6432c71ea9ac63f8e2599a8f9b81f
admin <admin@CR2-SRV1>
2021-11-19 01:13:56 +0600

    Add project files.

---
+++ Book.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission10
+{
+    class Book
+    {
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string ISBN { get; set; }
+        public string Language { get; set; }
+        public string Genre { get; set; }
+        public int CopyrightYear { get; set; }
+        public int Pages { get; set; }
+        public double Price { get; set; }
+        public int TotalCopies { get; set; }
+        public int AvailableCopies { get; set; }
+
+        public Book ( string title, List< string > authors , string isbn,
+        string language, string genre, int copyrightYear, int pages, double price, int totalCopies, int availableCopies)
+        {
+          Title = title;
+          Authors = authors;
+          ISBN = isbn;
+          Language = language;
+          Genre = genre;
+          CopyrightYear = copyrightYear;
+          Pages = pages;
+          Price = price;
+          TotalCopies = totalCopies;
+          AvailableCopies = availableCopies;
+        }
+        public override string ToString()
+        {
+            string str;
+            str = $"Title: {Title} " ;
+            foreach ( string author in Authors)
+        {
+            str += string .Format( $"\nAuthor: {author} " );
+        }
+            str += $"\nISBN: {ISBN} " ;
+            str += $"\nLanguage: { Language} \nGenre: { Genre} " ;
+            str += $"\nYear: { CopyrightYear} \nPages: { Pages} " ;
+            str += $"\nPrice: { Price} \nTotal Copies: { TotalCopies} " ;
+            str += $"\nAvailable Copies: { AvailableCopies} " ;
+            return str;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          List< string > authors1 = new List< string >() { "Hemingway" };
+          Book myFavoriteBook1 = new Book( "The Old Man and The Sea" , authors1, "123456789" , "English" , "Fiction" , 1952, 160, 19.99, 3, 3);
+          Console.WriteLine(myFavoriteBook1);
+          List< string > authors2 = new List< string >() { "Alan Moore" , "Dave Gibbons" };
+          Book myFavoriteBook2 = new Book( "Watchmen" , authors2, "223456789" , "English" , "Mystery" , 2019, 416, 29.99, 3, 3);
+          Console.WriteLine(myFavoriteBook2);
+          Console.ReadKey();
+        }
+    }
+}
